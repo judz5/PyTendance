ea07bd52c87ca933ce64a2c5e44337658510172f
Judson S <judz1105@gmail.com>
2022-04-20 07:16:15 -0700

    everyother frame

--- StudentImage/attendance.py
+++ StudentImage/attendance.py
@@ -17,6 +17,7 @@ print(classNames)
 def findEncodings(images):
     encodeList = []
     for img in images:
+        print(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
@@ -27,23 +28,30 @@ print('done')
 
 cap = cv2.VideoCapture(0)
 
+go = True
+
 while True:
-    success, img = cap.read(0)
-    imgS = cv2.resize(img,(0,0),None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    facesCurFrames = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrames)
+    if go:
 
-    for encodeFace, faceLoc, in zip(encodesCurFrame, facesCurFrames):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)
-        matchIndex = np.argmin(faceDis)
+        success, img = cap.read(0)
+        imgS = cv2.resize(img,(0,0),None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-            print(name)
+        facesCurFrames = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrames)
 
-        cv2.imshow('Webcam', img)
-        cv2.waitKey(1)    
+        for encodeFace, faceLoc, in zip(encodesCurFrame, facesCurFrames):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                print(name)
+
+    go = not go
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)    
